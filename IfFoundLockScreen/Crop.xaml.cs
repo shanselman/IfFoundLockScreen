25cac9d6f6b47b9c60aa902c4044efdedcf65a92
shanselman <shanselman@localhost>
2012-02-02 17:00:43 -0800

    Making AppHub rotate work

--- IfFoundLockScreen/Crop.xaml.cs
+++ IfFoundLockScreen/Crop.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using System.Windows.Media.Imaging;
 using System.IO;
+using Microsoft.Xna.Framework.Media;
+using ExifLib;
 
 namespace IfFoundLockScreen
 {
@@ -23,17 +25,14 @@ namespace IfFoundLockScreen
 
         }
 
-        
         // these two fields fully define the zoom state:
         private double TotalImageScale = 1d;
         private Point ImagePosition = new Point(0, 0);
-
 
         private const double MAX_IMAGE_ZOOM = 5;
         private Point _oldFinger1;
         private Point _oldFinger2;
         private double _oldScaleFactor;
-
 
         #region Event handlers
 
@@ -214,37 +213,8 @@ namespace IfFoundLockScreen
 
         private void ApplicationBarIconButton_Click(object sender, EventArgs e)
         {
-            //WriteableBitmap wb = new WriteableBitmap(480, 800);
-            
-            //CompositeTransform r = new CompositeTransform();
-            //r.ScaleX = TotalImageScale * 1.3;
-            //r.ScaleY = TotalImageScale * 1.3;
-            //r.TranslateX = ImagePosition.X-60;
-            //r.TranslateY = ImagePosition.Y-60;
-            //r.Transform(new Point(0,0));
-            
-
-            //wb.Render(ImageToCrop, r);
-            //wb.Invalidate();
-
-            // Get the size of the source image captured by the camera
+            // Get the size of the source image 
             BitmapImage obi = ImageToCrop.Source as BitmapImage;
-
-            //double cx = width;
-            //double cy = bi.PixelHeight * (cx / bi.PixelWidth);
-
-            //Image image = new Image();
-            //image.Source = bi;
-
-            //WriteableBitmap wb = new WriteableBitmap((int)cx, (int)cy);
-            //ScaleTransform transform = new ScaleTransform();
-            //transform.ScaleX = cx / bi.PixelWidth;
-            //transform.ScaleY = cy / bi.PixelHeight;
-            //wb.Render(image, transform);
-            //wb.Invalidate();
-
-            Point something = CropBorder.TransformToVisual(ImageToCrop).Transform(new Point(0, 0));
-
 
             double originalImageWidth = obi.PixelWidth;
             double originalImageHeight = obi.PixelHeight;
@@ -257,49 +227,188 @@ namespace IfFoundLockScreen
             double widthRatio = originalImageWidth / displayedWidth;
             double heightRatio = originalImageHeight / displayedHeight;
 
+            //Our goal is an image that is this size
             double goalHeight = 800;
             double goalWidth = 480;
 
-            // Create a new WriteableBitmap. The size of the bitmap is the size of the cropping rectangle
-            // drawn by the user, multiplied by the image size ratio.
+            //Get the Rect of our crop window
+            Point imageToCropP1 = CropBorder.TransformToVisual(ImageToCrop).Transform(new Point(0, 0));
+            Point imageToCropP2 = CropBorder.TransformToVisual(ImageToCrop).Transform(new Point(CropBorder.ActualWidth, CropBorder.ActualHeight));
 
-            Point p1 = CropBorder.TransformToVisual(ImageToCrop).Transform(new Point(0, 0));
-            Point p2 = CropBorder.TransformToVisual(ImageToCrop).Transform(new Point(CropBorder.ActualWidth, CropBorder.ActualHeight));
+            RectangleGeometry geo = new RectangleGeometry();
+            geo.Rect = new Rect(imageToCropP1, imageToCropP2);
 
             double viewPortWidthRatio = CropBorder.ActualWidth / goalWidth;
             double viewPortHeightRatio = CropBorder.ActualHeight / goalHeight;
 
-            //WriteableBitmap wb = new WriteableBitmap((int)(widthRatio * Math.Abs(p2.X - p1.X)), (int)(heightRatio * Math.Abs(p2.Y - p1.Y)));
             WriteableBitmap wb = new WriteableBitmap((int)goalWidth, (int)goalHeight); //size of our goal
 
             // Calculate the offset of the cropped image. This is the distance, in pixels, to the top left corner
             // of the cropping rectangle, multiplied by the image size ratio.
-            int xoffset = (int)(((p1.X < p2.X) ? p1.X : p2.X) * widthRatio);
-            int yoffset = (int)(((p1.Y < p2.Y) ? p1.Y : p2.X) * heightRatio);
+            int xoffset = (int)(((imageToCropP1.X < imageToCropP2.X) ? imageToCropP1.X : imageToCropP2.X) * widthRatio);
+            int yoffset = (int)(((imageToCropP1.Y < imageToCropP2.Y) ? imageToCropP1.Y : imageToCropP2.X) * heightRatio);
 
             CompositeTransform transform = new CompositeTransform();
-            //transform.ScaleX = TotalImageScale*widthRatio/.75;
-            //transform.ScaleY = TotalImageScale*heightRatio/.75;
             transform.ScaleX = TotalImageScale * (1 / viewPortWidthRatio);
             transform.ScaleY = TotalImageScale * (1 / viewPortHeightRatio);
-            transform.CenterX = something.X;
-            transform.CenterY = something.Y; 
-            transform.TranslateX = -(something.X);
-            transform.TranslateY = -(something.Y);
+            transform.CenterX = imageToCropP1.X;
+            transform.CenterY = imageToCropP1.Y;
+            transform.TranslateX = -(imageToCropP1.X);
+            transform.TranslateY = -(imageToCropP1.Y);
             wb.Render(ImageToCrop, transform);
             wb.Invalidate();
 
-            RectangleGeometry geo = new RectangleGeometry();
-            geo.Rect = new Rect(p1, p2);
-            ImageToCrop.Clip = geo;
-
+            //Actually save it
             ((App)App.Current).SaveCustomBackground(wb);
 
+            Storyboard sb = new Storyboard();
+            var duration = new Duration(TimeSpan.FromSeconds(0.750));
 
-            //this.NavigationService.GoBack();
+            var brushes = new string[] { "LeftDimBorder", "RightDimBorder", "TopDimBorder", "BottomDimBorder" };
+            foreach (var brush in brushes)
+            {
+                var fadeOut = new DoubleAnimation();
+                fadeOut.From = 0.6;
+                fadeOut.To = 1;
+                fadeOut.Duration = duration;
+
+                Storyboard.SetTarget(fadeOut, canvas.FindName(brush) as DependencyObject);
+                Storyboard.SetTargetProperty(fadeOut, new PropertyPath("Opacity"));
+                sb.Children.Add(fadeOut);
+            }
+            sb.Begin();
+            sb.Completed += (s, ea) => { 
+                
+                    //When we've darken the outside, now fly away!
+                    ImageToCrop.Clip = geo;
+
+                    Storyboard sb1 = new Storyboard();
+                    sb1.Duration = new Duration(TimeSpan.FromMilliseconds(400));
+                    
+                    var flyAway = new DoubleAnimationUsingKeyFrames();
+                    flyAway.KeyFrames.Add(new LinearDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0)), Value = 0 });
+                    flyAway.KeyFrames.Add(new LinearDoubleKeyFrame() { KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(400)), Value = 800 });
+                    
+                    Storyboard.SetTarget(flyAway, this.ContentPanel);
+                    Storyboard.SetTargetProperty(flyAway, 
+                        new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)")); 
+                    sb1.Children.Add(flyAway);
+                    sb1.Begin();
+                    sb1.Completed += (s2,ea2) => {
+                        this.NavigationService.GoBack();
+                    };
+            };
+            
         }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            //Did we get deep linked to?
+            // Get a dictionary of query string keys and values.
+            IDictionary<string, string> queryStrings = this.NavigationContext.QueryString;
+
+            if (queryStrings.ContainsKey("token"))
+            {
+                // Retrieve the picture from the media library using the token passed to the application.
+                MediaLibrary library = new MediaLibrary();
+                Picture picture = library.GetPictureFromToken(queryStrings["token"]);
+                
+                // Create a WriteableBitmap object and add it to the Image control Source property.
+                Stream s = picture.GetImage();
+                JpegInfo info = ExifLib.ExifReader.ReadJpeg(s, "magic.jpg");
+   
+                var _width = info.Width;
+                var _height = info.Height;
+                var _orientation = info.Orientation;
+                var _angle = 0;
+                
+                switch (info.Orientation)
+                {
+                    case ExifOrientation.TopLeft:
+                    case ExifOrientation.Undefined:
+                        _angle = 0;
+                        break;
+                    case ExifOrientation.TopRight:
+                        _angle = 90;
+                        break;
+                    case ExifOrientation.BottomRight:
+                        _angle = 180;
+                        break;
+                    case ExifOrientation.BottomLeft:
+                        _angle = 270;
+                        break;
+                }
+                //DEBUG
+                MessageBox.Show("Angle: " + _angle);
+
+                Stream resultStream;
+                if (_angle > 0d)
+                {
+                    resultStream = RotateStream(s, _angle);
+                }
+                else
+                {
+                    resultStream = s;
+                }
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.CreateOptions = BitmapCreateOptions.None;
+                bitmap.SetSource(resultStream);
+                this.ImageToCrop.Source = bitmap;
+            }
+            base.OnNavigatedTo(e);
+        }
+
+          private Stream RotateStream(Stream stream, int angle)
+          {
+              stream.Position = 0;
+              if (angle % 90 != 0 || angle < 0) throw new ArgumentException();
+              if (angle % 360 == 0) return stream;
+   
+              BitmapImage bitmap = new BitmapImage();
+              bitmap.SetSource(stream);
+              WriteableBitmap wbSource = new WriteableBitmap(bitmap);
+   
+              WriteableBitmap wbTarget = null;
+              if (angle % 180 == 0)
+              {
+                  wbTarget = new WriteableBitmap(wbSource.PixelWidth, wbSource.PixelHeight);
+              }
+              else
+              {
+                  wbTarget = new WriteableBitmap(wbSource.PixelHeight, wbSource.PixelWidth);
+              }
+
+              int width = wbSource.PixelWidth;
+              int height = wbSource.PixelHeight;
+              int targetWidth = wbTarget.PixelWidth;
+              int[] sourcePixels = wbSource.Pixels;
+              int[] targetPixels = wbTarget.Pixels;
+
+              for (int x = 0; x < wbSource.PixelWidth; x++)
+              {
+                  for (int y = 0; y < wbSource.PixelHeight; y++)
+                  {
+                      switch (angle % 360)
+                      {
+                          case 90:
+                              targetPixels[(height - y - 1) + x * targetWidth] = sourcePixels[x + y * width];
+                              break;
+                          case 180:
+                              targetPixels[(width - x - 1) + (height - y - 1) * width] = sourcePixels[x + y * width];
+                              break;
+                          case 270:
+                              targetPixels[y + (width - x - 1) * targetWidth] = sourcePixels[x + y * width];
+                              break;
+                      }
+                  }
+              }
+              MemoryStream targetStream = new MemoryStream();
+              wbTarget.SaveJpeg(targetStream, wbTarget.PixelWidth, wbTarget.PixelHeight, 0, 100);
+              return targetStream;
+           }
     }
 
-}
+    }
 
 
